9b3ab4f5114bc754ed6fbd83ff4e651fee583f82
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-06-06 11:32:41 +0900

    Create write navi function

---
+++ pages/api/navi/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  if (req.method === "POST") {
+    const {
+      body: { question, latitude, longitude },
+      session: { user },
+    } = req;
+    const navi = await client.navi.create({
+      data: {
+        question,
+        latitude,
+        longitude,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({ ok: true, navi });
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    method: ["POST"],
+    handler,
+  })
+);
--- pages/navi/write.tsx
+++ pages/navi/write.tsx
@@ -2,13 +2,60 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { Navi } from "@prisma/client";
+import useCoords from "@libs/client/useCoords";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  navi: Navi;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { user } = useUser();
+  const [write, { data, loading }] = useMutation<WriteResponse>("/api/navi");
+  const { latitude, longitude } = useCoords();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<WriteForm>();
+  const onVaild = (data: WriteForm) => {
+    if (loading) return;
+    write({ ...data, latitude, longitude });
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/navi/${data.navi.id}`);
+    }
+  }, [data, router]);
+
   return (
     <Layout canGoBack title="質問する">
-      <form className="space-y-4 p-4">
-        <TextArea required placeholder="周りの人に質問しよう!" />
-        <Button text={"送信"} />
+      <form className="space-y-4 p-4" onSubmit={handleSubmit(onVaild)}>
+        <TextArea
+          register={register("question", {
+            required: "質問内容を入力してください",
+            minLength: { value: 5, message: "5文字以上入力してください" },
+          })}
+          placeholder="周りの人に質問しよう!"
+          required
+        />
+        {errors ? (
+          <span className="block text-sm text-red-500">
+            {errors?.question?.message}
+          </span>
+        ) : null}
+        <Button text={loading ? "ローディング中" : "送信"} />
       </form>
     </Layout>
   );
