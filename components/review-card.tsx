3255932432d4f9dcf22be1dbdc86be6cfc6c8dbe
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-06-16 16:08:52 +0900

    Update delete update li

--- components/review-card.tsx
+++ components/review-card.tsx
@@ -200,25 +200,6 @@ export default function ReviewCard({
                   <div className="z-10 absolute right-0 w-max bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700">
                     <ul className="py-1 px-2 text-sm text-gray-700 dark:text-gray-200">
                       <li>
-                        <div className="px-2 py-2 rounded-lg font-medium hover:bg-slate-100 flex items-center space-x-2">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                            />
-                          </svg>
-                          <span>編集</span>
-                        </div>
-                      </li>
-                      <li>
                         <div
                           onClick={onDeleteClick}
                           className="px-2 py-2 text-red-500 rounded-lg font-medium hover:bg-red-50 flex items-center space-x-2"
