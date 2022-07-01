efea7774bc511b4e06f98ecde330e9a84a79119b
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-07-01 11:16:48 +0900

    Create google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { GA_TRACKING_ID, pageview } from "@libs/client/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <SWRConfig
       value={{
